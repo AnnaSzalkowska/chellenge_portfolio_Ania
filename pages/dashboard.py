12429db895b3df8cfc10a9926c3c6888b8f60b9b
AnnaSzalkowska <szalkowska.annaanna@gmail.com>
2023-11-20 02:23:57 +0100

    selector_tasks

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,21 +3,21 @@ from pages.base_page import BasePage
 
 class Dashboard(BasePage)
 
-    text_scouts_panel = "//h6[text()="Scouts Panel"]"
-    main_page_button =  "//*[@aria-label="menu"]"
+    text_scouts_panel = "//h6[text()='Scouts Panel']"
+    main_page_button =  "//*[@aria-label='menu']"
 
-    main_page_span = "//*[text()="Main page"]"
-    players_span = "//*[text()="Players"]"
-    polski_span = "//*[text()="Polski"]"
-    sign_out_span = "//*[text()="Sign out"]"
+    main_page_span = "//*[text()='Main page']"
+    players_span = "//*[text()='Players']"
+    polski_span = "//*[text()='Polski']"
+    sign_out_span = "//*[text()='Sign out']"
 
-    logo_scouts_panel = "//*[@title="Logo Scouts Panel"]"
-    contact_label = "//*[text()="Dev team contact"]"
-    scotus_panel_paragraph = "//p[text()="Player, match and report management panel."]"
-    text_shortcuts = "//h2[text()="Shortcuts"]"
-    add_player_label = "//*[text()="Add player"]"
+    logo_scouts_panel = "//*[@title='Logo Scouts Panel']"
+    contact_label = "//*[text()='Dev team contact']"
+    scotus_panel_paragraph = "//p[text()='Player, match and report management panel.']"
+    text_shortcuts = "//h2[text()='Shortcuts']"
+    add_player_label = "//*[text()='Add player']"
 
-    text_activity = "//h2[text()="Activity"]"
-    text_last_created_player = "//h6[text()="Last created player"]"
-    text_last_updated_player = "//h6[text()="Last updated player"]"
+    text_activity = "//h2[text()='Activity']"
+    text_last_created_player = "//h6[text()='Last created player']"
+    text_last_updated_player = "//h6[text()='Last updated player']"
     

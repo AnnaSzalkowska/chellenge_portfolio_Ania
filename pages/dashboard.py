4ac533391fcb4db18229eab06a36f7ca50aec302
AnnaSzalkowska <szalkowska.annaanna@gmail.com>
2023-11-22 23:26:49 +0100

    First_automatic_test,_asserts

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,9 @@
+import time
+
 from pages.base_page import BasePage
 
 
-class Dashboard(BasePage)
+class Dashboard(BasePage):
 
     text_scouts_panel = "//h6[text()='Scouts Panel']"
     main_page_button =  "//*[@aria-label='menu']"
@@ -20,4 +22,18 @@ class Dashboard(BasePage)
     text_activity = "//h2[text()='Activity']"
     text_last_created_player = "//h6[text()='Last created player']"
     text_last_updated_player = "//h6[text()='Last updated player']"
-    
+
+
+    expected_title = "Scouts panel - sign in"
+    login_url = "https://scouts-test.futbolkolektyw.pl/login"
+
+
+    def click_on_add_player_button(self):
+        self.click_on_the_element(self.add_player_button_xpath)
+
+
+    def title_of_page(self):
+        time.sleep(8)
+        assert self.get_page_title(self.login_url) == self.expected_title
+
+    add_player_button_xpath = "//*[text()='Add player']"
---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,53 @@
+import os
+import unittest
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from pages.dashboard import Dashboard
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    driver = None
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome()
+
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_log_in_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_the_page()
+        user_login.type_in_email('user03@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+        time.sleep(5)
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.get('https://scouts-test.futbolkolektyw.pl/login')
+
+
+    def test_assert_element_text(self):
+        xpath = "//*[text()='Scouts Panel']"
+        expected_text = "Scouts Panel"
+
+        element = self.driver.find_element(By.XPATH, xpath)
+        element_text = element.text
+
+        assert expected_text == element_text, f"Expected text: {expected_text}, Actual text: {element_text}"
+
+if __name__ == "__main__":
+    unittest.main()
+
+
